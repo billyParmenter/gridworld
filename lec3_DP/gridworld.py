7906e62b25c96c14cd5d5a4874e04d18be75748e
billyparmenter <bparmenter6387@conestogac.on.ca>
2023-10-05 16:33:05 -0400

    added grey states and rewards

--- lec3_DP/gridworld.py
+++ lec3_DP/gridworld.py
@@ -4,17 +4,19 @@ import numpy as np
 class GridWorld():
     def __init__(self, env_size):
         self.env_size = env_size
-        # TODO: Change the location of the terminal state and check how the optimal policy changes
-        # TODO: Add more than one terminal state (requires more changes in the code)
         self.terminal_state = (4, 4)
+        self.grey_states = [(2,2),(3,0),(4,0)]
 
         # Define the transition probabilities and rewards
-        self.actions = [(0, 1), (0, -1), (1, 0), (-1, 0)]  # Right, Left, Down, Up
-        self.action_description = ["Right", "Left", "Down", "Up"]
+        self.actions = [(0, 1), (1, 0), (1, 0), (-1, 0)]  # Right, Left, Down, Up
+        self.action_description = ["Right", "Down", "Down", "Up"]
 
         # Assign a vector of rewards for each of the states
         self.reward = np.ones((self.env_size, self.env_size))*-1
-        self.reward[self.terminal_state] = 0
+        self.reward[self.terminal_state] = 10
+        
+        for grey_state in self.grey_states:
+            self.reward[grey_state] = -5
 
     '''@brief Returns the next state given the chosen action and current state
     '''
